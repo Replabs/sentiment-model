3121bcf1e7bddcc1fda1e655cf9a64f96aa7bff5
Oliver Klingefjord <oliver@kry.se>
2022-08-19 06:41:42 +0200

    init

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, request
+from flask_cors import CORS
+from transformers import pipeline
+
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.logger.info("Starting app.")
+
+# Download the sentiment model.
+sentiment_task = pipeline(
+    'sentiment-analysis', model='./sentiment_model', tokenizer='./sentiment_model')
+
+app.logger.info("Loaded model.")
+
+sentiment_api_key = "f85661b0-fff7-4d1f-834d-770f9768fb36"
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+@app.route("/query", methods=['POST'])
+def embed():
+    body = request.get_json()
+
+    if 'text' not in body or 'api_key' not in body:
+        return {"error": "missing text"}, 400
+
+    api_key = body['api_key']
+
+    if api_key != sentiment_api_key:
+        return {"error": "invalid api key"}, 400
+
+    result = sentiment_task(body['text'])
+
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
